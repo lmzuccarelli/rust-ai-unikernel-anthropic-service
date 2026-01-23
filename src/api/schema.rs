da5e52711db51b96dca7c009ab219936e930fbb3
Luigi Mario Zuccarelli <luigizuccarelli@gmail.com>
2026-01-23 10:27:02 +0100

    Update to ensure api call aligment

--- src/api/schema.rs
+++ src/api/schema.rs
@@ -4,7 +4,7 @@ use serde_json::Value;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct AnthropicResponse {
+pub struct AnthropicChatCompletions {
     pub model: String,
     pub id: String,
     #[serde(rename = "type")]
--- src/handlers/ai.rs
+++ src/handlers/ai.rs
@@ -1,5 +1,5 @@
 use crate::MAP_LOOKUP;
-use crate::api::schema::AnthropicResponse;
+use crate::api::schema::AnthropicChatCompletions;
 use custom_logger as log;
 use http::{Method, Request, Response, StatusCode};
 use http_body_util::BodyExt;
@@ -14,28 +14,50 @@ pub async fn endpoints(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, 
     match *req.method() {
         Method::POST => match request {
             x if x.contains("/v1/chat/completions") => {
-                let data = req.into_body().collect().await?.to_bytes();
-                let result = process_post_call(data).await;
-                match result {
-                    Ok(content) => {
-                        *response.body_mut() = Full::from(content);
+                let headers = req.headers();
+                let res_access = headers.get("unikernel-access");
+                match res_access {
+                    Some(access) => {
+                        let access_value = access.to_str().unwrap_or_default();
+                        if access_value.is_empty() || access_value != "valid" {
+                            *response.status_mut() = StatusCode::FORBIDDEN;
+                            *response.body_mut() = Full::from("[endpoints] : access forbidden");
+                        } else {
+                            // all good, we can continue processing
+                            let data = req.into_body().collect().await?.to_bytes();
+                            let res_ppc = process_post_call(data).await;
+                            match res_ppc {
+                                Ok(content) => {
+                                    *response.body_mut() = Full::from(content);
+                                }
+                                Err(err) => {
+                                    log::error!("[endpoints] {}", err);
+                                    *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+                                    *response.body_mut() = Full::from(format!(
+                                        "[endpoints] error : {:?}\n",
+                                        err.source()
+                                    ));
+                                }
+                            }
+                        }
                     }
-                    Err(err) => {
-                        log::error!("[endpoints] {}", err);
-                        *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
-                        *response.body_mut() = Full::from(format!("error : {:?}\n", err.source()));
+                    _ => {
+                        log::error!("[endpoints] access forbidden ");
+                        *response.status_mut() = StatusCode::FORBIDDEN;
+                        *response.body_mut() = Full::from("[endpoints] : access forbidden\n");
                     }
-                }
+                };
             }
             &_ => {}
         },
         Method::GET => match request {
             x if x.contains("/v1/health") => {
-                let content = format!(
-                    r##"{{ "status":"ok", "appplication": "{}", "version": "{}" }}"##,
+                let mut content = format!(
+                    r##"{{ "status": "ok", "appplication": "{}", "version": "{}" }}"##,
                     env!("CARGO_PKG_NAME"),
                     env!("CARGO_PKG_VERSION"),
                 );
+                content.push_str("\n");
                 *response.body_mut() = Full::from(content);
             }
             &_ => {}
@@ -71,29 +93,37 @@ async fn process_post_call(data: Bytes) -> Result<String, Box<dyn std::error::Er
     let url = get_item("base_url")?;
     let token = get_item("api_key")?;
     log::debug!("[process_post_call] url {}", url);
-    log::info!(
+    log::debug!(
         "[process_post_call] data {}",
-        String::from_utf8(data.to_vec()).unwrap()
+        String::from_utf8(data.to_vec())?
     );
     let client_response = client
         .post(url)
-        .bearer_auth(token)
         .header("Content-Type", "application/json")
+        .header("x-api-key", token)
+        .header("anthropic-version", "2023-06-01")
         .body(data)
         .send()
         .await?;
+
     log::debug!("[process_post_call] status {}", client_response.status());
+    if client_response.status() != StatusCode::OK {
+        let response = client_response.bytes().await?;
+        let result = String::from_utf8(response.to_vec())?;
+        return Err(Box::from(result));
+    }
     let response = client_response.bytes().await?;
-    let chat_response: AnthropicResponse = serde_json::from_slice(&response)?;
+    let chat_response: AnthropicChatCompletions = serde_json::from_slice(&response)?;
+
     log::debug!(
-        "[process_post_call] output tokens {}",
-        chat_response.usage.output_tokens
-    );
-    log::debug!(
-        "[process_post_call] inpput tokens {}",
+        "[process_post_call] prompt tokens {}",
         chat_response.usage.input_tokens
     );
-    Ok(format!("\n{:?}\n", chat_response.content[0].text))
+    log::debug!(
+        "[process_post_call] completion tokens {}",
+        chat_response.usage.output_tokens
+    );
+    Ok(format!("\n{:?}\n", chat_response.content[0].text.clone()))
 }
 
 fn get_item(name: &str) -> Result<String, Box<dyn std::error::Error>> {
